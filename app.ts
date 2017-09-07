0e7ae56fecf1c61f0d6461f36e1061b87485c1aa
jnino <jnino@itfactoria.com>
2017-09-07 13:18:25 -0500

    Project Start

---
+++ app.ts
@@ -0,0 +1,29 @@
+function saludar(nombre: string){
+	console.log("hola " + nombre.toUpperCase())
+
+
+}
+
+var wolverine ={
+	nombre :"Logan"
+};
+
+saludar(wolverine.nombre);
+
+let dogger ="hey";
+if (true){
+	let dogger ="rub";
+}
+console.log("hola " +dogger);
+
+const WOMAN = "jan";
+
+let nombre: string ="mar";
+let edad: number =28;
+let guena: boolean = true;
+let coworker ={
+	name: "lucy",
+	age: 26
+
+}
+
