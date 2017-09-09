ba024df52e93cf2185f0e8397e75a6279187d351
jnino <jnino@itfactoria.com>
2017-09-09 08:58:48 -0500

    Interfaces development

--- app.js
+++ app.js
@@ -1,20 +1,70 @@
-function saludar(nombre) {
-    console.log("hola " + nombre.toUpperCase());
-}
-var wolverine = {
-    nombre: "Logan"
+"use strict";
+/********************************************************************************* */
+//VARIABLE DECLARATIONS
+/********************************************************************************* */
+//Destructuring Arrays
+var teams = ["America", "Nacional"];
+var team1 = teams[0], team2 = teams[1];
+console.log(team1, team2);
+//Destructuring Objects
+var car = {
+    marca: "chevy",
+    color: "rojo",
+    precio: 13
 };
-saludar(wolverine.nombre);
-var dogger = "hey";
-if (true) {
-    var dogger_1 = "rub";
+var marca = car.marca, color = car.color, precio = car.precio;
+console.log(marca, color, precio);
+/***********************************************************************************/
+//FUNCTIONS
+/************************************************************************************/
+//Parameters Types
+//Required parameters
+function ParametersRequired(a, b, c) {
+    return (a + b + c);
 }
-console.log("hola " + dogger);
-var WOMAN = "jan";
-var nombre = "mar";
-var edad = 28;
-var guena = true;
-var coworker = {
-    name: "lucy",
-    age: 26
+console.log("Parameros requeridos: " + ParametersRequired(1, 2, 3));
+//Default parameters
+function ParametersDefault(a, b, c) {
+    if (c === void 0) { c = 5; }
+    return (a + b + c);
+}
+console.log("Parameros x defecto: " + ParametersDefault(1, 3));
+console.log("Parameros x defecto: " + ParametersDefault(1, 2, 3));
+//Optional parameters
+function ParametersOptional(a, b, c) {
+    if (c) {
+        return (a + b + c);
+    }
+    else {
+        return (a + b);
+    }
+}
+console.log("Parameros opcionales: " + ParametersOptional(1, 2));
+//Function Types
+//Named function
+function NamedAdd(a, b) {
+    return a + b;
+}
+console.log("Named Function: " + NamedAdd(13, 9));
+//Anonymous function
+var AnomymousAdd = function (a, b) { return a + b; };
+console.log("Anonymous Function: " + AnomymousAdd(3, 4));
+//Arrow functions
+var Add = function (a, b) { return a + b; };
+console.log("Add Function: " + Add(13, 4));
+var ArrowAdd = function (a, b) { return a + b; };
+console.log("Arrow Add Function: " + ArrowAdd(10, 4));
+var demanda = {
+    nombre: "kraken",
+    genero: "rock",
+    exitoso: true,
+    cantar: function () { return "me encanta cantar!!!!"; }
 };
+var fabrica = {
+    nombre: "maluma",
+    genero: "regetonto",
+    exitoso: true,
+    cantar: function () { return "me encanta llorar!!!!"; }
+};
+console.log("demanda cantante" + demanda.nombre + "canta" + demanda.genero + demanda.cantar());
+console.log("fabrica cantante" + fabrica.nombre + "canta" + fabrica.genero + fabrica.cantar());
--- app.ts
+++ app.ts
@@ -1,29 +1,104 @@
-function saludar(nombre: string){
-	console.log("hola " + nombre.toUpperCase())
 
+/********************************************************************************* */
+//VARIABLE DECLARATIONS
+/********************************************************************************* */
+//Destructuring Arrays
 
-}
+let teams: string[] = ["America", "Nacional"];
+let [team1,team2] = teams;
+console.log(team1, team2);
 
-var wolverine ={
-	nombre :"Logan"
+//Destructuring Objects
+
+let car= {
+	marca: "chevy",
+	color: "rojo",
+	precio: 13
 };
 
-saludar(wolverine.nombre);
+let {marca,color,precio}=car;
+console.log(marca,color,precio);
 
-let dogger ="hey";
-if (true){
-	let dogger ="rub";
+
+/***********************************************************************************/
+//FUNCTIONS
+/************************************************************************************/
+
+//Parameters Types
+//Required parameters
+function ParametersRequired(a:number, b:number, c:number){
+	return (a+b+c);
 }
-console.log("hola " +dogger);
+console.log("Parameros requeridos: " +ParametersRequired(1,2,3));
 
-const WOMAN = "jan";
+//Default parameters
+function ParametersDefault(a:number, b:number, c:number=5){
+	return (a+b+c);
+}
+console.log("Parameros x defecto: " +ParametersDefault(1,3));
+console.log("Parameros x defecto: " +ParametersDefault(1,2,3));
 
-let nombre: string ="mar";
-let edad: number =28;
-let guena: boolean = true;
-let coworker ={
-	name: "lucy",
-	age: 26
+
+//Optional parameters
+function ParametersOptional(a:number, b:number, c?:number){
+	if (c){
+		return(a+b+c);
+	}
+	else{
+		return (a+b);
+	}
+	
+}
+console.log("Parameros opcionales: " +ParametersOptional(1,2));
+
+//Function Types
+//Named function
+function NamedAdd(a:number, b:number){
+	return a+b;
+}
+console.log("Named Function: " + NamedAdd(13,9));
+
+//Anonymous function
+let AnomymousAdd = function(a:number,b:number){return a+b}
+console.log("Anonymous Function: " + AnomymousAdd(3,4));
+
+//Arrow functions
+let Add = function(a:number,b:number){return a+b}
+console.log("Add Function: " + Add(13,4));
+
+let ArrowAdd = (a:number,b:number) => a+b;
+console.log("Arrow Add Function: " + ArrowAdd(10,4));
+
+/******************************************************************************/
+//INTERFACES
+/*****************************************************************************/
+
+interface cantante{
+	nombre: string,
+	genero: string,
+	exitoso: boolean,
+	cantar: () =>string
 
 }
+
+let demanda:cantante ={
+	nombre : "kraken",
+	genero : "rock",
+	exitoso : true,
+	cantar:()=>{return "me encanta cantar!!!!"}
+}
+
+let fabrica:cantante ={
+	nombre : "maluma",
+	genero : "regetonto",
+	exitoso : true,
+	cantar:()=>{return "me encanta llorar!!!!"}
+}
+
+console.log("demanda cantante" + demanda.nombre + "canta" + demanda.genero + demanda.cantar());
+console.log("fabrica cantante" + fabrica.nombre + "canta" + fabrica.genero + fabrica.cantar());
+
+
+
+
 
